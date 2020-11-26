5964e44d1b154b062148a6db838c22c0d4b381ac
William Santos <williamdsw@outlook.com>
2020-11-26 17:51:04 -0300

    Updating...

--- backend/src/main/java/com/williamdsw/semsys/mail/MailConstants.java
+++ backend/src/main/java/com/williamdsw/semsys/mail/MailConstants.java
@@ -1,21 +1,18 @@
 package com.williamdsw.semsys.mail;
 
-public class MailConstants 
-{
+public class MailConstants {
 	// FIELDS
-	
+
 	private static final String DEFAULT_SENDER = "sender@email.com";
 	private static final String DEFAULT_RECIPIENT = "recipient@email.com";
-	
+
 	// GETTERS
-	
-	public static String getDefaultSender () 
-	{
+
+	public static String getDefaultSender() {
 		return DEFAULT_SENDER;
 	}
-	
-	public static String getDefaultRecipient () 
-	{
+
+	public static String getDefaultRecipient() {
 		return DEFAULT_RECIPIENT;
 	}
 }
