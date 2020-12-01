21cae6341e7edd0dff89c53a3e2ee56301b75f9d
William Santos <williamdsw@outlook.com>
2020-12-01 12:16:14 -0300

    Fix envoriment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  // API: 'http://localhost:8080',
-  API: 'https://semsys-backend.herokuapp.com',
+  API: 'http://localhost:8080',
   BASE_URL: '/api',
   BUCKET_BASE_URL: 'https://semsys.s3-sa-east-1.amazonaws.com',
   DEFAULT_AVATAR_IMG: 'assets/images/avatar-blank.png',
